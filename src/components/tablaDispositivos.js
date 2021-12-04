897723d9613e9286f46937ad218cc6ae329e32ba
JulianP911 <j.padilla@uniandes.edu.co>
2021-12-04 13:23:15 -0500

    Internalización de las tablas done

--- src/components/tablaDispositivos.js
+++ src/components/tablaDispositivos.js
@@ -20,7 +20,7 @@ function TablaDispositivos (props) {
                         <tr key={index}>
                             <th scope="row">{index}</th>
                             <td>{d.id !== undefined? d.id: "N/A"}</td>
-                            <td>{d.name}</td>
+                            <td>{<FormattedMessage id={d.name} />}</td>
                             <td>{d.desired.value !== false && isNaN(d.desired.value.toString()) ? <FormattedMessage id={d.desired.value} />: isNaN(d.desired.value.toString())? <FormattedMessage id="off" />: d.desired.value}</td>
                         </tr>
                     );
